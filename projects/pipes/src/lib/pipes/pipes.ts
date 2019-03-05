b670725eda75393afb6fa00f3a82ccd19644d673
akshayr20 <akshay.rajput5394@gmail.com>
2019-03-05 12:01:55 +0530

    added capitalize first Pipe to the collection

--- projects/pipes/src/lib/pipes/pipes.ts
+++ projects/pipes/src/lib/pipes/pipes.ts
@@ -126,9 +126,21 @@ export class NotAvailablePipe implements PipeTransform {
   }
 }
 
+@Pipe({
+  name: 'capitalizeFirst'
+})
+export class CapitalizeFirstPipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    if (value === null) {
+      return;
+    }
+    return value.charAt(0).toUpperCase() + value.slice(1);
+  }
+}
+
 @NgModule({
   imports: [CommonModule],
-  exports: [SortByPipe, SearchPipe, HourAndMinConvertorPipe, TruncatePipe, SafePipe, CamelCaseToRegularWordPipe, NotAvailablePipe],
-  declarations: [SortByPipe, SearchPipe, HourAndMinConvertorPipe, TruncatePipe, SafePipe, CamelCaseToRegularWordPipe, NotAvailablePipe]
+  exports: [SortByPipe, SearchPipe, CapitalizeFirstPipe, HourAndMinConvertorPipe, TruncatePipe, SafePipe, CamelCaseToRegularWordPipe, NotAvailablePipe],
+  declarations: [SortByPipe, SearchPipe, CapitalizeFirstPipe, HourAndMinConvertorPipe, TruncatePipe, SafePipe, CamelCaseToRegularWordPipe, NotAvailablePipe]
 })
 export class PipeModule {}
